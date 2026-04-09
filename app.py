03da872510a198abbd77794d5c910c039fdf0a5a
NicoCodesCode <nicolasrmz08@gmail.com>
2026-04-09 17:20:18 -0500

    Deduplicate routes' logic

--- app.py
+++ app.py
@@ -1,111 +1,32 @@
 from flask import Flask, render_template, request
 
-from length_conversions import *
-from weight_conversions import *
-from temperature_conversions import *
+from conversion_functions import conversion_functions
 from utils import format_result
 
 app = Flask(__name__)
 
 
-@app.route("/")
-@app.route("/length", methods=["GET", "POST"])
-def length():
-    unit = "length"
+@app.route("/", defaults={"unit": "length"})
+@app.route("/<unit>", methods=["GET", "POST"])
+def converter(unit):
+    target_template = f"{unit}.html"
 
     if request.method == "POST":
-        length = float(request.form["length"])
+        value = float(request.form[unit])
         convert_from = request.form["convert-from"]
         convert_to = request.form["convert-to"]
         result = None
 
         if convert_from == convert_to:
             return render_template(
-                "length.html", unit=unit, result=format_result(length)
+                target_template, unit=unit, result=format_result(value)
             )
 
-        match convert_from:
-            case "inch":
-                result = format_result(convert_inch(length, convert_to))
-            case "foot":
-                result = format_result(convert_foot(length, convert_to))
-            case "yard":
-                result = format_result(convert_yard(length, convert_to))
-            case "mile":
-                result = format_result(convert_mile(length, convert_to))
-            case "millimeter":
-                result = format_result(convert_millimeter(length, convert_to))
-            case "centimeter":
-                result = format_result(convert_centimeter(length, convert_to))
-            case "meter":
-                result = format_result(convert_meter(length, convert_to))
-            case "kilometer":
-                result = format_result(convert_kilometer(length, convert_to))
+        conversion_function = conversion_functions[
+            f"convert_{convert_from.replace('-', '_')}"
+        ]
+        result = format_result(conversion_function(value, convert_to))
 
-        return render_template("length.html", unit=unit, result=result)
+        return render_template(target_template, unit=unit, result=result)
 
-    return render_template("length.html", unit=unit, result=None)
-
-
-@app.route("/weight", methods=["GET", "POST"])
-def weight():
-    unit = "weight"
-
-    if request.method == "POST":
-        weight = float(request.form["weight"])
-        convert_from = request.form["convert-from"]
-        convert_to = request.form["convert-to"]
-        result = None
-
-        if convert_from == convert_to:
-            return render_template(
-                "weight.html", unit=unit, result=format_result(weight)
-            )
-
-        match convert_from:
-            case "ounce":
-                result = format_result(convert_ounce(weight, convert_to))
-            case "pound":
-                result = format_result(convert_pound(weight, convert_to))
-            case "us-ton":
-                result = format_result(convert_us_ton(weight, convert_to))
-            case "milligram":
-                result = format_result(convert_milligram(weight, convert_to))
-            case "gram":
-                result = format_result(convert_gram(weight, convert_to))
-            case "kilogram":
-                result = format_result(convert_kilogram(weight, convert_to))
-            case "tonne":
-                result = format_result(convert_tonne(weight, convert_to))
-
-        return render_template("weight.html", unit=unit, result=result)
-
-    return render_template("weight.html", unit=unit, result=None)
-
-
-@app.route("/temperature", methods=["GET", "POST"])
-def temperature():
-    unit = "temperature"
-
-    if request.method == "POST":
-        temperature = float(request.form["temperature"])
-        convert_from = request.form["convert-from"]
-        convert_to = request.form["convert-to"]
-        result = None
-
-        if convert_from == convert_to:
-            return render_template(
-                "temperature.html", unit=unit, result=format_result(temperature)
-            )
-
-        match convert_from:
-            case "fahrenheit":
-                result = format_result(convert_fahrenheit(temperature, convert_to))
-            case "celsius":
-                result = format_result(convert_celsius(temperature, convert_to))
-            case "kelvin":
-                result = format_result(convert_kelvin(temperature, convert_to))
-
-        return render_template("temperature.html", unit=unit, result=result)
-
-    return render_template("temperature.html", unit=unit, result=None)
+    return render_template(target_template, unit=unit, result=None)
---
+++ conversion_functions.py
@@ -0,0 +1,24 @@
+from length_conversions import *
+from weight_conversions import *
+from temperature_conversions import *
+
+conversion_functions = {
+    "convert_inch": convert_inch,
+    "convert_foot": convert_foot,
+    "convert_yard": convert_yard,
+    "convert_mile": convert_mile,
+    "convert_millimeter": convert_millimeter,
+    "convert_centimeter": convert_centimeter,
+    "convert_meter": convert_meter,
+    "convert_kilometer": convert_kilometer,
+    "convert_ounce": convert_ounce,
+    "convert_pound": convert_pound,
+    "convert_us_ton": convert_us_ton,
+    "convert_milligram": convert_milligram,
+    "convert_gram": convert_gram,
+    "convert_kilogram": convert_kilogram,
+    "convert_tonne": convert_tonne,
+    "convert_fahrenheit": convert_fahrenheit,
+    "convert_celsius": convert_celsius,
+    "convert_kelvin": convert_kelvin,
+}
